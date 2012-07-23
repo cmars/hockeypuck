53228a4644dc30e6e2a50e8b717521963e417621
Casey Marshall <casey.marshall@gazzang.com>
2012-07-22 23:48:46 -0500

    Fix add w/ options test.

--- server_test.go
+++ server_test.go
@@ -24,7 +24,8 @@ import (
 	"github.com/bmizerany/assert"
 )
 
-func TestParseLookup(t *testing.T) {
+func TestGetKeyword(t *testing.T) {
+	// basic search
 	testUrl, err := url.Parse("/pks/lookup?op=get&search=alice")
 	assert.Equal(t, err, nil)
 	req := &http.Request{
@@ -39,7 +40,72 @@ func TestParseLookup(t *testing.T) {
 	assert.Equal(t, false, lookup.Exact)
 }
 
-func TestParseAdd(t *testing.T) {
+func TestGetFp(t *testing.T) {
+	// fp search
+	testUrl, err := url.Parse("/pks/lookup?op=get&search=0xdecafbad&options=mr,nm&fingerprint=on&exact=on")
+	assert.Equal(t, err, nil)
+	req := &http.Request{
+		Method: "GET",
+		URL: testUrl }
+	lookup, err := parseLookup(req)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, Get, lookup.Op)
+	assert.Equal(t, "0xdecafbad", lookup.Search)
+	assert.Equal(t, MachineReadable & lookup.Option, MachineReadable)
+	assert.Equal(t, NotModifiable & lookup.Option, NotModifiable)
+	assert.Equal(t, true, lookup.Fingerprint)
+	assert.Equal(t, true, lookup.Exact)
+}
+
+func TestIndex(t *testing.T) {
+	// index
+	testUrl, err := url.Parse("/pks/lookup?op=index&search=sharin") // as in, foo
+	assert.Equal(t, err, nil)
+	req := &http.Request{
+		Method: "GET",
+		URL: testUrl }
+	lookup, err := parseLookup(req)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, Index, lookup.Op)
+}
+
+func TestVindex(t *testing.T) {
+	// index
+	testUrl, err := url.Parse("/pks/lookup?op=vindex&search=bob") // as in, foo
+	assert.Equal(t, err, nil)
+	req := &http.Request{
+		Method: "GET",
+		URL: testUrl }
+	lookup, err := parseLookup(req)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, Vindex, lookup.Op)
+}
+
+func TestMissingSearch(t *testing.T) {
+	// index
+	testUrl, err := url.Parse("/pks/lookup?op=get")
+	assert.Equal(t, err, nil)
+	req := &http.Request{
+		Method: "GET",
+		URL: testUrl }
+	_, err = parseLookup(req)
+	// error without search term
+	assert.NotEqual(t, err, nil)
+}
+
+func TestNoSuchOp(t *testing.T) {
+	// index
+	testUrl, err := url.Parse("/pks/lookup?op=barrelroll")
+	assert.Equal(t, err, nil)
+	req := &http.Request{
+		Method: "GET",
+		URL: testUrl }
+	_, err = parseLookup(req)
+	// Unknown operation
+	assert.NotEqual(t, err, nil)
+}
+
+func TestAdd(t *testing.T) {
 	testUrl, err := url.Parse("/pks/add")
 	assert.Equal(t, err, nil)
 	postData := make(map[string][]string)
@@ -52,4 +118,34 @@ func TestParseAdd(t *testing.T) {
 	assert.Equal(t, err, nil)
 	assert.Equal(t, "sus llaves aqui", add.Keytext)
 	assert.Equal(t, NoOption, add.Option)
+}
+
+func TestAddOptions(t *testing.T) {
+	testUrl, err := url.Parse("/pks/add?options=mr")
+	assert.Equal(t, err, nil)
+	postData := make(map[string][]string)
+	postData["keytext"] = []string{ "sus llaves estan aqui" }
+	postData["options"] = []string{ "mr" }
+	req := &http.Request{
+		Method: "POST",
+		URL: testUrl,
+		Form: url.Values(postData) }
+	add, err := parseAdd(req)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "sus llaves estan aqui", add.Keytext)
+	assert.Equal(t, MachineReadable & add.Option, MachineReadable)
+	assert.Equal(t, NotModifiable & add.Option, NoOption)
+}
+
+func TestAddMissingKey(t *testing.T) {
+	testUrl, err := url.Parse("/pks/add")
+	assert.Equal(t, err, nil)
+	postData := make(map[string][]string)
+	req := &http.Request{
+		Method: "POST",
+		URL: testUrl,
+		Form: url.Values(postData) }
+	_, err = parseAdd(req)
+	// error without keytext
+	assert.NotEqual(t, err, nil)
 }
