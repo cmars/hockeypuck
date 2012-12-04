69abc66314141ef23646c058538b524ef12e9d7f
Casey Marshall <casey.marshall@gazzang.com>
2012-12-03 21:55:44 -0600

    * === added directory instroot, etc., scripts/crossbuild.sh: * scripts/crossbuild.sh:   - Add dist tarball creation.

--- scripts/crossbuild.sh
+++ scripts/crossbuild.sh
@@ -7,12 +7,13 @@
 . $HOME/local/bin/golang-crosscompile
 
 if [ -z "$1" ]; then
-	PLATFORMS="darwin-386 darwin-amd64 freebsd-386 freebsd-amd64 linux-amd64 windows-386 windows-amd64"
+	PLATFORMS="darwin-386 darwin-amd64 freebsd-386 freebsd-amd64 linux-386 linux-amd64 windows-386 windows-amd64"
 else
 	PLATFORMS=$1
 fi
 
 GOPATH=$(pwd)/gopath
+rm -rf $(GOPATH)
 
 APPS="launchpad.net/hockeypuck/cmd/hockeypuck-mgo launchpad.net/hockeypuck/cmd/hockeypuck-load"
 
@@ -24,5 +25,28 @@ do
 		go-$platform install $app
 	done
 done
+
+BUILD=$(pwd)/build
+DIST=$(pwd)/dist
+rm -rf $BUILD
+mkdir -p $DIST
+
+host_platform=$(go env GOOS)-$(go env GOARCH)
+
+for platform in $PLATFORMS;
+do
+	platform_bin=$(echo $platform | sed 's/-/_/')
+	tar -C instroot -cvf $DIST/hockeypuck-$platform.tar .
+	mkdir -p $BUILD/$platform/usr/bin
+	if [[ $platform != $host_platform ]]; then
+		cp -r $GOPATH/bin/$platform_bin/* $BUILD/$platform/usr/bin
+	else
+		cp -r $GOPATH/bin/hockeypuck-* $BUILD/$platform/usr/bin
+	fi
+	tar -C $BUILD/$platform -rvf $DIST/hockeypuck-$platform.tar ./usr/bin
+	gzip -9f $DIST/hockeypuck-$platform.tar
+done
+
+#rm -rf $BUILD
 
 exit 0
