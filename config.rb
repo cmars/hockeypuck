bff633900515f5f22ab794c14657f095350f41bb
Casey Marshall <cmars@cmarstech.com>
2013-09-14 11:29:43 -0500

    octopress init & deploy

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/cmars/hockeypuck/"
+http_images_path = "/cmars/hockeypuck/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/cmars/hockeypuck/fonts"
+css_dir = "public/cmars/hockeypuck/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
