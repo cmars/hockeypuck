c137b1cf8b3810eed3be5ec3b433b20340d2bd8b
Casey Marshall <cmars@cmarstech.com>
2013-10-13 15:34:29 -0500

    * doc/Makefile, doc/deploy.sh, doc/pages/index.txt, doc/pages/install-   source.txt, doc/pages/install-ubuntu.txt, doc/pages/pre-   populating.txt:   - Revised documentation, add http://hockeypuck.github.io doc deploy     script.

---
+++ doc/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+if [ ! -d "output" ]; then
+	echo "Documentation site not created, run 'make' to build it."
+	exit 1
+fi
+
+cd output
+rm -rf .git
+git init
+#git checkout -b gh-pages
+#git remote add origin git@github.com:cmars/hockeypuck.git
+git remote add origin git@github.com:hockeypuck/hockeypuck.github.io.git
+git add *
+git commit -m "Generated Hockeypuck documentation at $(date '+%y%m%d-%H%M%S')"
+#git push -f origin gh-pages
+git push -f origin master
