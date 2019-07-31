a5ae13b6af9b62b97be6b7716872a1567bb32c1d
Paul Collins <paul.collins@canonical.com>
2019-08-01 09:12:34 +1200

    use ID instead of KeyID in KeyAdded and KeyReplaced structs, to match Digest
    also lastID instead of lastKeyID, which is perilously close to lastKey

--- src/hockeypuck/hkp/storage/storage.go
+++ src/hockeypuck/hkp/storage/storage.go
@@ -114,7 +114,7 @@ type KeyChange interface {
 }
 
 type KeyAdded struct {
-	KeyID  string
+	ID     string
 	Digest string
 }
 
@@ -127,13 +127,13 @@ func (ka KeyAdded) RemoveDigests() []string {
 }
 
 func (ka KeyAdded) String() string {
-	return fmt.Sprintf("key 0x%s with hash %s added", ka.KeyID, ka.Digest)
+	return fmt.Sprintf("key 0x%s with hash %s added", ka.ID, ka.Digest)
 }
 
 type KeyReplaced struct {
-	OldKeyID  string
+	OldID     string
 	OldDigest string
-	NewKeyID  string
+	NewID     string
 	NewDigest string
 }
 
@@ -146,7 +146,7 @@ func (kr KeyReplaced) RemoveDigests() []string {
 }
 
 func (kr KeyReplaced) String() string {
-	return fmt.Sprintf("key 0x%s with hash %s replaced key 0x%s with hash %s", kr.NewKeyID, kr.NewDigest, kr.OldKeyID, kr.OldDigest)
+	return fmt.Sprintf("key 0x%s with hash %s replaced key 0x%s with hash %s", kr.NewID, kr.NewDigest, kr.OldID, kr.OldDigest)
 }
 
 type KeyNotChanged struct{}
@@ -197,7 +197,7 @@ func UpsertKey(storage Storage, pubkey *openpgp.PrimaryKey) (kc KeyChange, err e
 		if err != nil {
 			return nil, errgo.Mask(err)
 		}
-		return KeyAdded{KeyID: pubkey.KeyID(), Digest: pubkey.MD5}, nil
+		return KeyAdded{ID: pubkey.KeyID(), Digest: pubkey.MD5}, nil
 	} else if err != nil {
 		return nil, errgo.Mask(err)
 	}
@@ -205,7 +205,7 @@ func UpsertKey(storage Storage, pubkey *openpgp.PrimaryKey) (kc KeyChange, err e
 	if pubkey.UUID != lastKey.UUID {
 		return nil, errgo.Newf("upsert key %q lookup failed, found mismatch %q", pubkey.UUID, lastKey.UUID)
 	}
-	lastKeyID := lastKey.KeyID()
+	lastID := lastKey.KeyID()
 	lastMD5 := lastKey.MD5
 	err = openpgp.Merge(lastKey, pubkey)
 	if err != nil {
@@ -216,7 +216,7 @@ func UpsertKey(storage Storage, pubkey *openpgp.PrimaryKey) (kc KeyChange, err e
 		if err != nil {
 			return nil, errgo.Mask(err)
 		}
-		return KeyReplaced{OldKeyID: lastKeyID, OldDigest: lastMD5, NewKeyID: lastKey.KeyID(), NewDigest: lastKey.MD5}, nil
+		return KeyReplaced{OldID: lastID, OldDigest: lastMD5, NewID: lastKey.KeyID(), NewDigest: lastKey.MD5}, nil
 	}
 	return KeyNotChanged{}, nil
 }
