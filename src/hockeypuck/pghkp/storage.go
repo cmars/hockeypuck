02a6d8d2589b119ba2c9bd00c087e375b3b1bc7d
Paul Collins <paul.collins@canonical.com>
2019-08-19 16:13:02 +1200

    consistent spacing when specifying index columns

--- src/hockeypuck/pghkp/storage.go
+++ src/hockeypuck/pghkp/storage.go
@@ -71,8 +71,8 @@ FOREIGN KEY (rfingerprint) REFERENCES keys(rfingerprint)
 
 var crIndexesSQL = []string{
 	`CREATE INDEX IF NOT EXISTS keys_rfp ON keys(rfingerprint text_pattern_ops);`,
-	`CREATE INDEX IF NOT EXISTS keys_ctime ON keys (ctime);`,
-	`CREATE INDEX IF NOT EXISTS keys_mtime ON keys (mtime);`,
+	`CREATE INDEX IF NOT EXISTS keys_ctime ON keys(ctime);`,
+	`CREATE INDEX IF NOT EXISTS keys_mtime ON keys(mtime);`,
 	`CREATE INDEX IF NOT EXISTS keys_keywords ON keys USING gin(keywords);`,
 	`CREATE INDEX IF NOT EXISTS subkeys_rfp ON subkeys(rsubfp text_pattern_ops);`,
 }
