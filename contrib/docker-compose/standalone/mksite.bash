7861901e6185d4d62da34ef391af697a606ce812
Casey Marshall <git@cmars.tech>
2019-09-01 15:24:15 -0500

    Add standalone docker deploy & supporting scripts, config.

---
+++ contrib/docker-compose/standalone/mksite.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+HERE=$(cd $(dirname $0); pwd)
+set -eu
+
+[ ! -e "$HERE/site.profile" ]
+
+POSTGRES_PASSWORD=$(head -c 30 /dev/urandom | base32 -w0)
+cat >$HERE/site.profile <<EOF
+FQDN=hockeypuck.io
+POSTGRES_USER="hkp"
+POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
+EOF
+chmod 600 $HERE/site.profile
