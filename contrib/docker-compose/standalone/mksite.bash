3a7d7772e2020939e84e4b08d3111c8e19f712b3
Casey Marshall <git@cmars.tech>
2019-09-01 19:54:43 -0500

    Export site env vars; uncomment nginx & certbot

--- contrib/docker-compose/standalone/mksite.bash
+++ contrib/docker-compose/standalone/mksite.bash
@@ -7,9 +7,9 @@ set -eu
 
 POSTGRES_PASSWORD=$(head -c 30 /dev/urandom | base32 -w0)
 cat >$HERE/site.profile <<EOF
-FQDN=hockeypuck.io
-EMAIL=hockeypuck@\$FQDN
-POSTGRES_USER="hkp"
-POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
+export FQDN=hockeypuck.io
+export EMAIL=hockeypuck@hockeypuck.io
+export POSTGRES_USER="hkp"
+export POSTGRES_PASSWORD="${POSTGRES_PASSWORD}"
 EOF
 chmod 600 $HERE/site.profile
