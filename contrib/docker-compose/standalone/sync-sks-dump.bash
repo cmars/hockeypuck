ff651fa1a3423fde4156f0c946c4de9c7e1b0d08
Casey Marshall <git@cmars.tech>
2019-09-01 23:29:53 -0500

    Fix rsync url

--- contrib/docker-compose/standalone/sync-sks-dump.bash
+++ contrib/docker-compose/standalone/sync-sks-dump.bash
@@ -2,4 +2,4 @@
 
 set -eu
 
-docker-compose exec import-keys /bin/bash -c "cd /import; rsync -avr rsync:rsync.cyberbits.eu/sks/dump ."
+docker-compose exec import-keys /bin/bash -c "cd /import; rsync -avr rsync://rsync.cyberbits.eu/sks/dump ."
